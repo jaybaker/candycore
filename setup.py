038460c19484694c20d92eed1b6adbff3c97220f
Jay Baker <jay@jaybakerweb.com>
2016-08-21 19:07:48 -0500

    Prep for PyPi.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'candycore',
+  packages = ['candycore'],
+  version = '0.1',
+  description = 'Common utilities for candywriter',
+  author = 'Jay Baker',
+  author_email = 'jbaker.work@gmail.com',
+  url = 'https://github.com/jaybaker/candycore',
+  download_url = 'https://github.com/jaybaker/candycore/tarball/0.1',
+  keywords = ['candywriter'],
+  classifiers = [],
+)
