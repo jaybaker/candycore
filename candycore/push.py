94798f44593a2d55c2197b915477313029ef7839
Jay Baker <jay@jaybakerweb.com>
2016-12-15 14:41:29 -0600

    Added push notification.

---
+++ candycore/push.py
@@ -0,0 +1,37 @@
+from logging import debug, info, error
+
+from google.appengine.api import urlfetch
+
+import json
+
+import settings
+
+PARSE_URL = 'https://api.parse.com/1/push'
+FBASE_URL = 'https://fcm.googleapis.com/fcm/send'
+
+def notify(topic, msg, priority='high'):
+    headers = {"Authorization": "key=%s" % (settings.FBASE['api_key']),
+               "Content-Type": "application/json"}
+
+    to = '/topics/%s' % topic
+    data = dict(to=to, priority=priority,
+            notification=dict(body=msg))
+
+    # convenience function
+    def fetch():
+        return urlfetch.fetch(url=FBASE_URL, payload=json.dumps(data), 
+                headers=headers, method=urlfetch.POST, deadline=30)
+
+    # send push notification
+    try:
+        resp = fetch()
+    except:
+        # retry once
+        debug('retrying push')
+        resp = fetch()
+
+    if resp.status_code != 200:
+        debug('status code: %s' % resp.status_code)
+        debug('content: %s' % resp.content)
+        debug('headers: %s' % resp.headers)
+        error('failed to send push notification')
